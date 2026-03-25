51622a77f9d636f08f2d194ca564c967d7bc323c
karixcomputers <karixcomputers@gmail.com>
2026-03-26 00:10:20 +0200

    fixes 13

--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -40,7 +40,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({ 
   storage: storage,
   fileFilter: fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 } // Limită de 5MB pe fișier
+  limits: { fileSize: 50 * 1024 * 1024 } // Limită de 5MB pe fișier
 });
 
 // --- RUTA NOUĂ: POST UPLOAD (Folosită de Admin Inventory) ---
